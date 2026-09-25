6d2f874713aba04bbff17a08f4edd56f3ad526ab
Shaheen Gaming <kingshoaib7865@gmail.com>
2026-09-25 18:03:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function openDetails(name, role, condition, price, desc, phone) {
+function openDetails(imgSrc, name, role, condition, price, desc, phone) {
+    document.getElementById('modal-img').src = imgSrc;
     document.getElementById('modal-name').innerText = name;
     document.getElementById('modal-role').innerText = role;
     document.getElementById('modal-condition').innerText = condition;
     document.getElementById('modal-price').innerText = price;
     document.getElementById('modal-desc').innerText = desc;
 
-    // WhatsApp Direct Link Setup
-    const text = encodeURIComponent(`سلام! مجھے ${name} خریدنا ہے۔ اس کی قیمت ${price} ہے نا؟`);
+    // WhatsApp Message
+    const text = encodeURIComponent(`سلام! مجھے ${name} خریدنا ہے۔ اس کی قیمت ${price} ہی ہے نا؟`);
     document.getElementById('modal-wa-link').href = `https://wa.me/${phone}?text=${text}`;
 
+    // Modal Display
     document.getElementById('detailsModal').style.display = 'flex';
 }
 
@@ -16,7 +18,7 @@ function closeDetails() {
     document.getElementById('detailsModal').style.display = 'none';
 }
 
-// Close Modal when clicking outside content
+// Modal closing when clicking outside the box
 window.onclick = function(event) {
     const modal = document.getElementById('detailsModal');
     if (event.target == modal) {
